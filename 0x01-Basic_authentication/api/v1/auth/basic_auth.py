90dee40603004c10c67f6c7945ded30f2c14532e
Emmanuel Olagoke <emma.goke05@gmail.com>
2023-08-09 00:13:06 +0100

    Added the auth class that will manage the Basic authentication

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+This script contains the class that will implement
+Basic authentication.
+"""
+from .auth import Auth
+
+
+class BasicAuth(Auth):
+    """ Contains Basic Authentication Implementation. """
+    pass
