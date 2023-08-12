6f2e24f11effaf317a283443fc78bce8a652bf1e
Emmanuel Olagoke <emma.goke05@gmail.com>
2023-08-13 00:15:52 +0100

    Create the create_session class for creating the session id for an authenticated user

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -4,8 +4,20 @@ This script contains the class that will implement
 Session authentication.
 """
 from .auth import Auth
+from uuid import uuid4
 
 
 class SessionAuth(Auth):
     """ Contains Session Authentication Implementation. """
-    pass
+
+    def __init__(self):
+        """ This is the constructor of the session class. """
+        self.user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """ This session create the session id for the user """
+        if user_id is None or type(user_id) != str:
+            return None
+        session_id = uuid4()
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
