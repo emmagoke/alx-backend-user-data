4c06ef12c064ab34c03b68801b44623cac9cb02d
Emmanuel Olagoke <emma.goke05@gmail.com>
2023-08-13 01:04:23 +0100

    Create the before_request to handle if a user is not logined in

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -35,11 +35,13 @@ def before_request() -> str:
     excluded_list = [
         '/api/v1/status/',
         '/api/v1/unauthorized/',
-        '/api/v1/forbidden/'
+        '/api/v1/forbidden/',
+        '/api/v1/auth_session/login/'
     ]
     if auth.require_auth(request.path, excluded_list):
         #  request.path is not part of this list do nothing
-        if auth.authorization_header(request) is None:
+        if auth.authorization_header(request) is None and\
+                auth.session_cookie(request) is None:
             abort(401)
         if auth.current_user(request) is None:
             abort(403)
