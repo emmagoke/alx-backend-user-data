99e9f41c5c86f2b11229a5da79470ad02b2d8dd9
Emmanuel Olagoke <emma.goke05@gmail.com>
2023-08-13 01:28:04 +0100

    Created current_user which returns a User instance based on a cookie value

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -5,6 +5,7 @@ Session authentication.
 """
 from .auth import Auth
 from uuid import uuid4
+from models.user import User
 
 
 class SessionAuth(Auth):
@@ -24,3 +25,9 @@ class SessionAuth(Auth):
         """ This method return the user_id based on the session_id """
         if type(session_id) == str:
             return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """ returns a User instance based on a cookie value """
+        session_id = self.session_cookie(request)
+        user_id = self.user_id_for_session_id(session_id)
+        return User.get(user_id)
