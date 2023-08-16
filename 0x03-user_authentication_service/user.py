221d15b2dccf2df76eea50fb5bb04e5cf0a056cf
Emmanuel Olagoke <emma.goke05@gmail.com>
2023-08-16 10:12:21 +0100

    Added the User Model

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+This script contains the user Model for the user authentication
+"""
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, Integer
+
+
+Base = declarative_base()
+
+
+class User(Base):
+    """ This is the user model class. """
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=False)
+    reset_token = Column(String(250), nullable=False)
