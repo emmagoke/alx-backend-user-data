62066508e7e939653e8ab3f0b0932fcc99ef247b
Emmanuel Olagoke <emma.goke05@gmail.com>
2023-08-17 11:39:51 +0100

    Created than user function to handle the /users route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,9 +2,11 @@
 """
 This script contains route and handles user request using flask
 """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, abort
+from auth import Auth
 
 
+Auth = Auth()
 app = Flask(__name__)
 
 
@@ -12,6 +14,18 @@ app = Flask(__name__)
 def home():
     """ This handles request sent to the home route. """
     return jsonify({"message": "Bienvenue"}), 200
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def users():
+    """ This handles the post request of the users route. """
+    email = request.form.get('email')
+    password = request.form.get('password')
+    try:
+        user_register = Auth.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"}), 200
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
