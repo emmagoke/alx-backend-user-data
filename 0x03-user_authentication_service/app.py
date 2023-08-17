59dbb0eebce710d6c9a7ab543fe62ea27b9c35e5
Emmanuel Olagoke <emma.goke05@gmail.com>
2023-08-17 13:28:33 +0100

    Created the /sessions that logs the user in

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -28,5 +28,17 @@ def users():
         return jsonify({"message": "email already registered"}), 400
 
 
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login():
+    """ This function handles user login. """
+    email, password = request.form.get('email'), request.form.get('password')
+    if Auth.valid_login(email, password):
+        session_id = Auth.create_session(email)
+        response = jsonify({"email": email, "message": "logged in"})
+        response.set_cookie("session_id", session_id)
+        return response
+    abort(401)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
