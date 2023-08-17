f862345385720e2dcde38eba139a801fbe80e33b
Emmanuel Olagoke <emma.goke05@gmail.com>
2023-08-17 12:09:32 +0100

    Created the valid_login that validate the user details

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -33,3 +33,15 @@ class Auth:
             except NoResultFound:
                 user = self._db.add_user(email, _hash_password(password))
                 return user
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """ This method check if the user details are correct """
+        try:
+            user = self._db.find_user_by(email=email)
+            if user:
+                encoded_password = password.encode('utf-8')
+                if bcrypt.checkpw(encoded_password, user.hashed_password):
+                    return True
+            return False
+        except NoResultFound:
+            return False
