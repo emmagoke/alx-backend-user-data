b5d15ae7ed3fd7ddf263a91cf203f98f2791304c
Emmanuel Olagoke <emma.goke05@gmail.com>
2023-08-17 15:43:23 +0100

    Created the get_reset_password_token method to generate and update a reset_token for a user

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -78,3 +78,13 @@ class Auth:
         by setting it to None. """
         self._db.update_user(user_id, session_id=None)
         return None
+
+    def get_reset_password_token(self, email: str) -> str:
+        """ This method generate and return the user's reset token. """
+        try:
+            user = self._db.find_user_by(email=email)
+            reset_token = _generate_uuid()
+            self._db.update_user(user.id, reset_token=reset_token)
+            return reset_token
+        except NoResultFound:
+            reise ValueError
